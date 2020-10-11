4ed50de9058697eaaf84cbd0e5c5277e17b14437
abhivikrant <vikrantabhishek72@gmail.com>
2020-10-12 00:29:16 +0530

    Added script.

---
+++ Abhishek_Vikrant_Portfolio/index.html/script.js
@@ -0,0 +1,12 @@
+$(function () {
+  $("nav").css("display", "none").removeClass("hidden");
+  $(window).scroll(function () {
+    // find the trigger point.
+    var point = $("#intro h1").offset().top - 30;
+    if ($(window).scrollTop() > point) {
+      $("nav").slideDown();
+    } else {
+      $("nav").slideUp();
+    }
+  });
+});
